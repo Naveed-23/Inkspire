01b59c1b945a239de641afa06cf6a435e3664d1f
Naveed-23 <naveedbh23@gmail.com>
2025-03-25 11:41:08 +0530

    added zod schema on fe

--- apps/inkspire-frontend/components/AuthPage.tsx
+++ apps/inkspire-frontend/components/AuthPage.tsx
@@ -6,11 +6,24 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { SignupSchema } from "@repo/common-folder/types";
+
+type AuthFormData = z.infer<typeof SignupSchema>;
 
 export function AuthPage({ isSignin }: { isSignin: boolean}){
-    const { reset, register, handleSubmit, formState: { errors }} = useForm();
+    const { reset, register, handleSubmit, setError, formState: { errors }} = useForm<AuthFormData>();
 
-    function onSubmit(data: any) {
+    function onSubmit(data: AuthFormData) {
+        const parsedData = SignupSchema.safeParse(data);
+
+        if(!parsedData.success){
+            console.log("parsedData", parsedData);
+            parsedData.error.errors.forEach(err => {
+                setError(err.path[0] as keyof AuthFormData, { message: err.message})
+            });
+            return;
+        }
         console.log("Form Data", data);
     }    
     
@@ -42,17 +55,17 @@ export function AuthPage({ isSignin }: { isSignin: boolean}){
                 {!isSignin && <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="name">Name</Label>
                   <Input id="name" {...register("name", { required: !isSignin })} placeholder="John Snow" />
-                  {errors.name && <p className="text-red-500 text-sm">Name is required</p>}
+                  {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
                 </div>}
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="name">Email</Label>
                   <Input id="email" {...register("email", {required: "Email is required"})} placeholder="johnsnow@gmail.com" />
-                  {errors.email && <p className="text-red-500 text-sm">Email is required</p>}
+                  {errors.email && <p className="text-red-500 text-sm">{errors.email?.message}</p>}
                 </div>
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="name">Password</Label>
                   <Input id="password" {...register("password", {required: "Password is required"})} placeholder="nothing123" type="password" />
-                  {errors.password && <p className="text-red-500 text-sm">Password is required</p>}
+                  {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
                 </div>
               </div>
           <CardFooter className="flex justify-between mt-4 p-[-4px]">
--- packages/common-folder/src/types.ts
+++ packages/common-folder/src/types.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
 
 export const SignupSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(3),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(5, "Password must be at least 5 characters"),
     name: z.string(),
     photo: z.string().optional()
 });
 
 export const SigninSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(3),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(5, "Password must be at least 5 characters"),
 });
 
 export const CreateRoomSchema = z.object({
